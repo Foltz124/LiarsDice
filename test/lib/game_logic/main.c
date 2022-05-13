582157dd522c67028791fee3b48a2dc35a266730
Foltz124 <Foltz.124@osu.edu>
2022-05-12 22:00:37 -0400

    LD-2: Fixed build issues after last commit

---
+++ test/lib/game_logic/main.c
@@ -0,0 +1,18 @@
+#include <check.h>
+#include <stdlib.h>
+
+#include "TestPokerUtils.h"
+
+int main(void) {
+    int numberFailed = 0;
+    Suite* mainSuite = suite_create("Core");
+    SRunner* suiteRunner;
+
+    suiteRunner = srunner_create(mainSuite);
+    srunner_add_suite(suiteRunner, pokerUtilsSuite());
+
+    srunner_run_all(suiteRunner, CK_NORMAL);
+    numberFailed = srunner_ntests_failed(suiteRunner);
+    srunner_free(suiteRunner);
+    return (numberFailed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
